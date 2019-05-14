6b8f9d2b62aa552925ef0e8f0e9037f3639382fa
Bryan Bishop <bryan.bishop@pearson.com>
2019-05-14 10:58:40 -0500

    Add solution to rna transcription :fire:

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,15 @@
 def to_rna(dna_strand):
-    pass
+    result = ""
+    if dna_strand == result:
+        return(result)
+    else:
+        for i in dna_strand:
+            if i == "A":
+                result += "U"
+            if i == "T":
+                result += "A"
+            if i == "C":
+                result += "G"
+            if i == "G":
+                result += "C"
+        return(result)

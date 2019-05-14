3dff9817308fe74084c8fc2f77225ab816c66078
Bryan Bishop <bryan.bishop@pearson.com>
2019-05-14 11:03:49 -0500

    Clean up return :star:

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -5,5 +5,4 @@ def to_rna(dna_strand):
     else:
         in_table = "GCTA"
         out_table = "CGAU"
-        result = dna_strand.translate(str.maketrans(in_table, out_table))
-        return(result)
+        return(dna_strand.translate(str.maketrans(in_table, out_table)))
